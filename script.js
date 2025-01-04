56ff19ea7782d902854653eb9307baab26b252f3
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-01-04 21:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function stringChop(str, size) {
   // your code here
+  if (str.length === 0) return [];
+  if (str.length < size) return [str];
+  const chunks = [];
+  let count = 0;
+  let s = "";
+  let i;
+  for (i = 0; i < str.length; ++i) {
+    s += str[i];
+    ++count;
+    if (count === size) {
+      chunks.push(s);
+      s = "";
+      count = 0;
+    } 
+  }
+  if (count !== 0) chunks.push(s);
+  return chunks;
 }
-
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
